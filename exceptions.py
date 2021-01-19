5c2051e5d83caf280edb4d49811b154546a00393
Jake Ledoux <contactjakeledoux@gmail.com>
2021-01-19 11:33:08 -0500

    Improved exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,5 +1,18 @@
-class AccountNotFoundError(BaseException):
-    pass
+from typing import Union
 
-class InsufficientBalanceError(BaseException):
-    pass
+
+class AccountNotFoundError(Exception):
+    """ When querying accounts returns nothing.
+    """
+    def __init__(self, message: Union[str, None]):
+        message = message or 'Failed to find account with matching parameters.'
+        super(AccountNotFoundError, self).__init__(message)
+
+
+class InsufficientBalanceError(Exception):
+    """ When an account tries to use funds it doesn't possess.
+    """
+    def __init__(self, message: Union[str, None]):
+        message = message or 'The account has insufficent balance to complete \
+                this action.'
+        super(AccountNotFoundError, self).__init__(message)
