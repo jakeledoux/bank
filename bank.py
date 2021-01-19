f54c2804d14764b7417b42b695c1535cdf1da856
Jake Ledoux <contactjakeledoux@gmail.com>
2021-01-19 10:07:28 -0500

    TUI concept

--- bank.py
+++ bank.py
@@ -1,1 +1,8 @@
-from models import User, Transaction
+# from models import Account, Transaction
+from prompt_toolkit.shortcuts import button_dialog
+
+choice = button_dialog(title='WAMPA BANK', text='Hello, world!',
+                       buttons=[('Log in', 0),
+                                ('Sign up', 1),
+                                ('Exit', 2)]).run()
+print(choice)
